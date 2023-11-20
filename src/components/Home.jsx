3579268f739fc6e4b497b14796fd0c77ed867c69
Krishna <kanavkrishna05@gmail.com>
2023-11-20 19:19:09 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-// import { motion } from "framer-motion";
 import gif from '../Photos/remove.bg.gif';
 
 export default function BgRemover() {
